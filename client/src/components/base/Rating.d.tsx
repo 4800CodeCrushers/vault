7da44756665893b4865607207115e17b0ceb5af5
jchu62 <89236068+jchu62@users.noreply.github.com>
2023-10-14 18:30:40 -0700

    Attempt to add new library to facilitate easier rating system

---
+++ client/src/components/base/Rating.d.tsx
@@ -0,0 +1,9 @@
+// Rating.d.ts
+// See file Rating.jsx for more information
+
+import { FC } from "react";
+import { RatingComponentProps } from "react-rating";
+
+declare const NewRating: FC<RatingComponentProps>;
+
+export default NewRating;
