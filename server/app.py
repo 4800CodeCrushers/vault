f799c6a6094927b39e658ebbd23fd87542d90a26
Cameron Ross <cameron.edward.ross@gmail.com>
2023-10-17 10:36:31 -0700

    update

--- server/app.py
+++ server/app.py
@@ -25,10 +25,6 @@ app.before_request(beforeRequest)
 def home():
     return render_template('index.html')
 
-# Load the config file
-f = open('config.json')
-data = load(f)
-
 # Run the app
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', debug=False, port=data['port'])
+	app.run(host='0.0.0.0', debug=False)
