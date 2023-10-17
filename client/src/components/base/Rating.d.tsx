31b5aeaf656caab1a86f31ca3a9a944d74cd28c0
Cameron Ross <cameron.edward.ross@gmail.com>
2023-10-16 19:09:01 -0700

    Revert "Merge pull request #1 from 4800CodeCrushers/GamePanel"
    This reverts commit 8d080a71693032ea4a1b31abd0d9a4ea79faa875, reversing
    changes made to 7561e3fef38dd07bd8884986c19b4bf9d2e4f32c.

--- client/src/components/base/Rating.d.tsx
+++
@@ -1,9 +0,0 @@
-// Rating.d.ts
-// See file Rating.jsx for more information
-
-import { FC } from "react";
-import { RatingComponentProps } from "react-rating";
-
-declare const NewRating: FC<RatingComponentProps>;
-
-export default NewRating;
--- client/src/components/base/Rating.jsx
+++
@@ -1,7 +0,0 @@
-// Necessary file because this rating library is 4 years old
-// and hasn't been updated for newer versions of React
-import React from "react";
-import Rating from "react-rating";
-
-const NewRatting = ({ ...props }) => <Rating {...props} />;
-export default NewRatting;
