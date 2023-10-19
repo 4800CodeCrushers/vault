37720d60bb4e2d5064d3b085a5e4c36e3366ffb0
Cameron Ross <cameron.edward.ross@gmail.com>
2023-10-19 04:02:17 -0700

    update

--- client/src/components/screens/LandingScreen.tsx
+++ client/src/components/screens/LandingScreen.tsx
@@ -63,7 +63,7 @@ function LandingScreen(props: LandingScreenProps) {
     <div style = {styles.screen}>
       {/* Title */}
       <Text style={{textAlign: 'center', marginTop: 40}}>Welcome to</Text>
-      <Text style={styles.title}>MyGamesVault.com!!!</Text>
+      <Text style={styles.title}>MyGamesVault.com</Text>
       {/* Logo */}
       <img style = {{width: 200, height: 200, margin: 25,  alignSelf: 'center', pointerEvents: 'none'}} src = {require('../../assets/logo.png')}/>
       {/* Input fields */}
